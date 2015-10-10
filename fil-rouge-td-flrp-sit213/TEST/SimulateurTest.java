93393f89776c2b955f900f175adbe4502c2266f8
tbfilrouge <th.dumur@gmail.com>
2015-10-10 17:42:44 +0200

    TESTS avancés

--- fil-rouge-td-flrp-sit213/TEST/SimulateurTest.java
+++ fil-rouge-td-flrp-sit213/TEST/SimulateurTest.java
@@ -19,6 +19,20 @@ public class SimulateurTest {
 	String tab9[]={"-mess","4","-ampl", "12.0" , "12"};
 	String tab10[]={"-mess","4","-ampl", "12.0" , "12.0"};
 	String tab11[]={"-mess","4","-ampl", "10.0" , "12.0"};
+	String tab12[]={"-mess","4","-seed","abdc"};
+	String tab13[]={"-mess","4","-seed","1"};
+	
+	
+	
+	String ok0[]={"-mess","4"};
+	String ok1[]={"-mess","101010101010101000111"};
+	String ok2[]={"-mess","4","-nbEch", "30"};
+	String ok3[]={"-mess","4","-form","RZ"};
+	String ok4[]={"-mess","4","-form","NRZ"};
+	String ok5[]={"-mess","4","-form","NRZT"};
+	
+	
+	
 	
 	@Test
 	public void testSimulateur0() throws ArgumentsException {
@@ -70,15 +84,58 @@ public class SimulateurTest {
 		Simulateur A = new Simulateur(tab10);
 	}
 	
-	@Test
+	@Test(expected = ArgumentsException.class)
 	public void testSimulateur11() throws ArgumentsException {
 		Simulateur A = new Simulateur(tab11);
 	}
+	
+	@Test(expected = ArgumentsException.class)
+	public void testSimulateur12() throws ArgumentsException {
+		Simulateur A = new Simulateur(tab12);
+	}
+	
+	@Test(expected = ArgumentsException.class)
+	public void testSimulateur13() throws ArgumentsException {
+		Simulateur A = new Simulateur(tab13);
+	}
+		
 	
 	@Test
 	public void testCalculTauxErreurBinaire() {
 		fail("Not yet implemented");
 	}
-
+// TEST DES CAS NORMAUX 
+	
+	@Test
+	public void testok0() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok0);
+	}
+	
+	@Test
+	public void testok1() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok1);
+	}
+	
+	@Test
+	public void testok2() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok2);
+	}
+	
+	@Test
+	public void testok3() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok3);
+	}
+	
+	@Test
+	public void testok4() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok4);
+	}
+	
+	@Test
+	public void testok5() throws ArgumentsException {
+		Simulateur A = new Simulateur(ok5);
+	}
+	
+	
 }
 
---
+++ fil-rouge-td-flrp-sit213/TEST/sources/SourceAleatoireTest.java
@@ -0,0 +1,22 @@
+package sources;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class SourceAleatoireTest {
+	SourceAleatoire A;
+	SourceAleatoire B;
+
+	@Test
+	public void testEmettre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSourceAleatoire() {
+		A = new SourceAleatoire(5);
+		B = new SourceAleatoire(5);
+		
+	}
+
+}
